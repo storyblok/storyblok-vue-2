8a3b3bb255b058aa990db6135862b9de27313429
Alex Jover <alexjovermorales@gmail.com>
2022-02-23 20:18:56 +0100

    feat: add StoryblokComponent and useStoryblok

---
+++ lib/StoryblokComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <component
+    :is="blok.component"
+    v-bind="{ ...$props, ...$attrs }"
+    v-on="$listeners"
+  ></component>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+    },
+  },
+};
+</script>
