803528fd27d60549872a121455eddf96ce97cfc8
Alex Jover <alexjovermorales@gmail.com>
2022-02-28 20:30:23 +0100

    fix: separate useStoryblok for tree-shaking

--- playground/main.js
+++ playground/main.js
@@ -3,12 +3,10 @@ import { StoryblokVue, apiPlugin } from "@storyblok/vue-2";
 import App from "./App.vue";
 
 Vue.use(StoryblokVue, {
-  accessToken: "wANpEQEsMYGOwLxwXQ76Ggtt",
+  accessToken: "OurklwV5XsDJTIE1NJaD2wtt",
   // bridge: false,
   // apiOptions: {  },
   use: [apiPlugin], // use it only if you need it
 });
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+new Vue({ render: (h) => h(App) }).$mount("#app");
