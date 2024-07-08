1ce12bb0ed2a65033427692b912d466b73395cb8
alvarosabu <alvaro.saburido@gmail.com>
2024-07-08 15:58:05 +0200

    feat: deprecation warnings

--- lib/src/StoryblokComponent.vue
+++ lib/src/StoryblokComponent.vue
@@ -14,4 +14,8 @@ export default {
     },
   },
 };
+
+console.warn(
+  "Warning ⚠️: This package will be deprecated on August 31st, 2024. Please use the `@storyblok/vue` package instead. Check the migration guide here: https://dev.to/storyblok/official-deprecation-announcement-storyblok-vue-2-nuxt-2-sdks-n1g"
+);
 </script>
